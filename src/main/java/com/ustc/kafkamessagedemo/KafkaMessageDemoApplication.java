e20dcfa1aa4dc8d0c78c8ed73fd5fd26bb4c1d55
miaoyangyang <miaoyangyang1998@foxmail.com>
2023-07-30 16:19:43 +0800

    首次提交，创建仓库

---
+++ src/main/java/com/ustc/kafkamessagedemo/KafkaMessageDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ustc.kafkamessagedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMessageDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaMessageDemoApplication.class, args);
+    }
+
+}
